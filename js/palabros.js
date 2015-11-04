70bbbac1a336fdc7846a3316af550f8fb19547dd
pysdp <xurxo.a@gmail.com>
2015-11-04 15:34:50 +0100

    Actualización

---
+++ js/palabros.js
@@ -0,0 +1,15 @@
+var data =[
+  {
+    "palabra": "Lesión cerebral traumática (LCT)",
+    "definicion": "Es una lesión cerebral causada, frecuentemente, por un traumatismo craneal cerrado debido a una brusca aceleración o desaceleración del movimiento espontáneo de la cabeza en accidentes de tráfico o caídas.",
+  },
+  {
+    "palabra": "Los conflictos",
+    "definicion": "Son situaciones en las que dos o más personas entran en oposición o desacuerdo porque sus posiciones, intereses, necesidades, deseos o valores son incompatibles, o son percibidos como incompatibles, donde juegan un papel muy importante las emociones y sentimientos y donde la relación entre las partes en conflicto puede salir fortalecida o deteriorada en función de cómo sea el proceso de resolución del conflicto.",
+  },
+  {
+    "palabra": "Resiliencia (psicológica)",
+    "definicion": "Capacidad humana que permite a las personas y a las comunidades, a pesar de atravesar situaciones adversas, lograr salir adelante y aún más, enriquecidas por la experiencia",
+  }
+]
+		var entry = data[Math.floor(Math.random()*data.length)];
