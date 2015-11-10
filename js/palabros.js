2a9541e829bf5808957f1a3465164a2d5f7c59ec
pysdp <xurxo.a@gmail.com>
2015-11-10 15:12:33 +0100

    Actualización y nuevo test

--- js/palabros.js
+++ js/palabros.js
@@ -10,6 +10,94 @@ var data =[
   {
     "palabra": "Resiliencia (psicológica)",
     "definicion": "Capacidad humana que permite a las personas y a las comunidades, a pesar de atravesar situaciones adversas, lograr salir adelante y aún más, enriquecidas por la experiencia",
+  },
+  {
+    "palabra": "Provención del conflicto (J. Burton)",
+    "definicion": "<p>Es un proceso de intervención antes de las crisis que nos lleva a:</p><ul><li>Una explicación adecuada del conflicto, incluyendo su dimensión humana.</li><li>Un conocimiento de los cambios necesarios para eliminar sus causas.</li><li>Una promoción de condiciones adecuadas para el favorecimiento de relaciones cooperativas que disminuyan el riesgo de nuevos enfrentamientos, aprendiendo a tratar y solucioar las contradicciones antes de que lleguen a convertirse en antagonismos</li></ul>",
+  },
+  {
+    "palabra": "Las fases de un conflicto (Lederach)",
+    "definicion": "<ol><li>Conflicto latente</li><li>Confrontación o escalada</li><li>Negociación</li><li>Resolución del conflicto</li></ol>",
+  },
+  {
+    "palabra": "Variables personales ante la gestión de un conflicto",
+    "definicion": "<p><strong>Sinergia</strong> o preocupación por la relación con la otra parte. El grado en que se está dispuesto a esforzarse para satisfacer las necesidades/intereses de la otra parte.</p><p><strong>Asertividad</strong> o preocupación por la negociación misma (resultados). El grado en que se está dispuesto a esfozarse para satisfacer los propios intereses y necesidades.</p>",
+  },
+  {
+    "palabra": "Empatía",
+    "definicion": "<p>Es la identificación mental y afectiva de una persona con el estado de ánimo de otra. También se conoce como inteligencia interpersonal, al ser la capacidad cognitiva de sentir, en un contexto común, lo que un individuo diferente puede percibir.</p><p> Algunas habilidades y aptitudes que condicionan la empatía son:</p><ol><li>Suspensión de juicio o propensión a pensar antes de actuar</li><li>Interés/respeto/consideración hacia el otro</li><li>Postura interactiva o disposición para entrar en contacto</li><li>Tolerancia ante la ambigüedad</li><li>Complejidad cognitiva o capacidad de gestionar costumbres sociales diferentes</li><li>Capacidad para diferenciar estereotipos de individuos</li><li>Relativización de las opiniones personales.</li></ol>",
+  },
+  {
+    "palabra": "Las Emociones",
+    "definicion": "Son reacciones subjetivas al ambiente, acompañadas de cambios orgánicos de origen innatos (incremento de la tasa cardíaca y de la respiración, tensión musuclar, etc.), que se encuentran influídas por la experiencia y tienen una función adaptativa que nos permite ponernos en alerta ante determinadas situaciones que implican peligro, amenaza, frustración, etc. Las categorías básicas de las emociones son: miedo, sospresa, aversión, ira, tristeza y alegría.",
+  },
+  {
+    "palabra": "Axioma 1 de la Comunicación de Watzlawick",
+    "definicion": "<strong> Es imposible no comunicar</strong>: Cualquier situación en la que participan dos o más personas implica el intercambio de mensajes. Toda conducta (lenguaje, silencios, inactividad, movimientos...), de forma intencional o no, en una situación de interacción  puede ser interpretado y es, por tanto, un mensaje. Todo lo que hacen las personas en presencia de otros tiene el valor de un mensaje.",
+  },
+  {
+    "palabra": "Axioma 2 de la Comunicación de Watzlawick",
+    "definicion": "<strong>Toda comunicación tiene un nivel de contenido y un nivel relacional</strong>; Toda comunicación posee un contenido (lo que decimos) y una relación (a quién y cómo se lo decimos). Por tanto, la comunicación no sólo trasmite información sino que, al mismo tiempo, impone una conducta o un comportamiento, definiendo el tipo de relación que se establece con el interlocutor.",
+  },
+  {
+    "palabra": "Axioma 3 de la Comunicación de Watzlawick",
+    "definicion": "La naturaleza de una relación y por tanto el sentido de los mensajes depende de la puntuación de las secuencias de comunicación que cada participante establece;.",
+  },
+  {
+    "palabra": "Axioma 4 de la Comunicación de Watzlawick",
+    "definicion": "Las personas utilizan tanto la comunicación digital como analógica. La comunicación humana es tando verbal como no verbal.",
+  },
+  {
+    "palabra": "Axioma 5 de la Comunicación de Watzlawick",
+    "definicion": "<p><strong>Todo intercambio comunicativo queda incluído en la categoría de interacción simétrica o complementaria según esté basado en la igualdad o en la diferencia.</strong></p><p>La interacción simétrica ocurre cuando las personas intercambian el mismo tipo de comportamiento, de modo que existen idénticos deberes y derechos. Tanto uno como el otro puede, indistintamente, tomar decisiones, criticar y comentar la conducta del otro, dar consejos y proponer alternativas,etc.</p><p>La interacción complementaria es característica de aquelas relaciones en que se intercambian diferentes tipos de comportamiento. En una relación complementaria hay dos posiciones distintas: un miembro ocupa una posición superior o primaria, mientras que el otro ocupa la posición inferior o secundaria.",
+  },
+  {
+    "palabra": "Resiliencia (Gotberg, 1995)",
+    "definicion": "La capacidad humana para enfrentar, sobreponerse y ser fortalecido o transformado por experiencias de adversidad.",
+  },
+  {
+    "palabra": "Resiliencia (Vanistendael, 1994)",
+    "definicion": "La capacidad del inividuo pra hacer las cosas bien pese a las circunstancias adversas. Esto implica una capacidad de resistencia y una facultad de construcción positiva. Así, la resiliencia implica dos componentes: la resistencia frente a la destrucción; y, por otra parte, la capacidad para una construcción positiva pese a circunstancias difíciles.",
+  },
+  {
+    "palabra": "Resiliencia (Munist y Cols., 1998)",
+    "definicion": "La capacidad del ser humano para hacer frente a las adversidades de la vida, superarlas y ser transformado positivamente por ellas.",
+  },
+  {
+    "palabra": "'Conflicto total no cero' o 'Conflicto de motivos variados'",
+    "definicion": "Conflicto clásico que ilustra la mayoría de los conflictos de la vida humana en los que las alternativas, los motivos, las situaciones, las presiones, las posibles soluciones... son mucho más complejas que en los llamados 'conflictos de suma cero', aquellos que se definen por presentar sólo dos posibilidades, ganar o perder y en los que lo que uno pierde es lo mismo que lo que el otro gana.",
+  },
+  {
+    "palabra": "Sociedad (Funcionalismo)",
+    "definicion": "Conjunto de elementos interdependientes que cumplen funciones específicas y que se encuentran regidos por un sistema de valores común.",
+  },
+  {
+    "palabra": "Poder de coerción",
+    "definicion": "Capacidad de administrar castigos y que conlleva la vigilancia de A sobre B para evitar que B escape de la situación en la que recibe el castigo.",
+  },
+  {
+    "palabra": "Poder de recompensa",
+    "definicion": "Cuando A puede proporcionar gratificaciones o reducir o suprimir sinsabores o frustraciones. B debe de percibir el poder de A.",
+  },
+  {
+    "palabra": "Poder legítimo",
+    "definicion": "Es el derecho que B le otorga a A de influir sobre él en una relación de autoridad donde el comportamiento y el estatus están consensuados. Una relación docente puede ser de este tipo, o una relación entre jefe y subordinado.",
+  },
+  {
+    "palabra": "Poder de referencia",
+    "definicion": "B se identifica con A y lo valora porque A posee carisma, prestigio o una serie de cualidades que B admira.",
+  },
+  {
+    "palabra": "Poder de competencia",
+    "definicion": "Cuando A posee unos conocimientos particulares. Es el experto en quien B confia.",
+  },
+  {
+    "palabra": "Asertividad de oposición",
+    "definicion": "Se refiere a la respuesta que se da en situaciones de discusión o enfrentamiento con el objetivo de evitar que el otro consiga sus objetivos. Consisten en oposiciones o comentarios de rechazo de la conducta o expresión inadecuada del otro, con la intención de conseguir un comportamiento más aceptable para el futuro.",
+  },
+  {
+    "palabra": "Asertividad de aceptación",
+    "definicion": "Supone la capacidad para hacer comentarios positivos, elogios, aprobaciones... de la conducta del otro o de alguna de sus características cuando está justificado.",
   }
 ]
 		var entry = data[Math.floor(Math.random()*data.length)];
